c95b6b3178b6a6ddd558b5b00763ce76893cfd9b
Daniel Łaś <daniel.las@empirica.io>
2024-03-03 05:20:54 +0100

    Integers sum benchmarks

---
+++ src/main/java/tech/dl/sos/RunBenchmarks.java
@@ -0,0 +1,18 @@
+package tech.dl.sos;
+
+import org.openjdk.jmh.runner.RunnerException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class RunBenchmarks {
+
+	public static void main(String[] args) {
+		try {
+			new IntegerSumBenchmark().runBenchmark();
+		} catch (RunnerException e) {
+			log.error("Benchmark run failed", e);
+		}
+	}
+
+}
